a74048583f85f7b82ed7f5200d2a3f8ae3c891c1
margga88 <margga88@yandex.com>
2025-07-07 16:07:29 -0400

    JS file updated

--- scripts.js
+++ scripts.js
@@ -1,17 +1,22 @@
 let x = document.getElementById("share");
 let y = document.getElementById("article-author");
-let card = document.getElementById("article-card");
 
-function shareFunction() {  
 
-  if (x.style.display === "none" && y.style.display === "grid") {
+function shareFunction() {
+  let w = document.documentElement.clientWidth || window.innerWidth;
+
+  var targetWidthOne = 768;
+  var targetWidthTwo = 1440;  
+
+  if (w <= targetWidthOne && x.style.display === "none" && y.style.display === "grid") {
     x.style.display = "block";
     y.style.display = "none";
   } 
-      
+        
   else {
     x.style.display = "none";
     y.style.display = "grid";
   } 
 
 }
+
