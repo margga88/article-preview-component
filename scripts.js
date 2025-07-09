956402d69d6d254f5079cc0d54aec15868e2c15d
margga88 <margga88@yandex.com>
2025-07-09 17:08:48 -0400

    Files updated. Desktop 1024 done and working. Awaiting for desktop 1440

--- scripts.js
+++ scripts.js
@@ -1,19 +1,27 @@
-let x = document.getElementById("share");
-let y = document.getElementById("article-author");
-
+let a = document.getElementById("share");
+let b = document.getElementById("article-author");
+let c = document.getElementById("share-desktop");
+let d = document.getElementById("share-pointer");
 
 function shareFunction() {
   let w = document.documentElement.clientWidth || window.innerWidth;
-  var targetWidth = 768; 
+  var targetWidth = 768;  
 
-  if (w <= targetWidth && x.style.display === "none" && y.style.display === "grid") {
-    x.style.display = "block";
-    y.style.display = "none";
+  if (w <= targetWidth && a.style.display === "none" && b.style.display === "grid") {
+    a.style.display = "block";
+    b.style.display = "none";
   } 
-        
+
+  else if (w > targetWidth && c.style.display === "none" && d.style.display === "none" ) {
+    c.style.display = "block";
+    d.style.display = "block";    
+  } 
+      
   else {
-    x.style.display = "none";
-    y.style.display = "grid";
+    a.style.display = "none";
+    b.style.display = "grid";
+    c.style.display = "none";
+    d.style.display = "none";    
   } 
 
 }
